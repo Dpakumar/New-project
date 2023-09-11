42fe05df72670148a89e5a2ffb90feb50600ac68
Deepak kumar <danusdeepak@gmail.com>
2023-09-11 22:13:34 +0530

    Update new 1.py

--- new 1.py
+++ new 1.py
@@ -102,7 +102,6 @@ model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)  
 
-# Model Evaluation
 from sklearn.metrics import mean_squared_error, r2_score
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
